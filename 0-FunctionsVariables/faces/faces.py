0a07a063750724b59b7e266b45f733f62b054c91
FHawthorne <flynn.hawthorne@education.nsw.gov.au>
2026-02-15 01:35:32 +0000

    CS50 problems finished

---
+++ 0-FunctionsVariables/faces/faces.py
@@ -0,0 +1,13 @@
+def convert(text):
+    text = text.replace(":)", "🙂")
+    text = text.replace(":(", "🙁")
+    return text
+
+
+def main():
+    text = input("please enter your face: ")
+    text = convert(text)
+    print(text)
+
+
+main()
