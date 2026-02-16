fac126bba2b16100e882795f0a4387ba1b2a5043
FHawthorne <flynn.hawthorne@education.nsw.gov.au>
2026-02-16 22:36:22 +0000

    Upload completed interpreter.py

---
+++ 1-Conditionals/interpreter/interpreter.py
@@ -0,0 +1,13 @@
+expression = input("expression: ")
+x, y, z = expression.split(" ")
+x = int(x)
+z = int(z)
+
+if y == "+":
+    print(x + z)
+elif y == "-":
+    print(x - z)
+elif y == "*":
+    print(x * z)
+elif y == "/":
+    print(x / z)
