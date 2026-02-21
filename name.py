fcc71b3a57be7dd5dbb1d771ce904c7e81dea590
FHawthorne <flynn.hawthorne@education.nsw.gov.au>
2026-02-21 11:34:30 +0000

    Upload complete emojize.py

--- name.py
+++ name.py
@@ -3,6 +3,9 @@ import sys
 if len(sys.argv) < 2:
     sys.exit("Too few arguments")
 
-for arg in sys.argv:
+for arg in sys.argv[1:]:
     print("hello, my name is", arg)
-print("hello my name is", sys.argv[1])
+
+
+if __name__ == "__name__":
+    main()
