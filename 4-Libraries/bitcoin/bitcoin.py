c028601e614d6710adeba88b3085617b5d9e607e
FHawthorne <flynn.hawthorne@education.nsw.gov.au>
2026-02-23 01:24:48 +0000

    Upload complete bitcoin.py

---
+++ 4-Libraries/bitcoin/bitcoin.py
@@ -0,0 +1,35 @@
+import requests
+import sys
+
+
+def main():
+    n = get_n()
+    price = get_bitcoin()
+    final = n * price
+    final = round(final, 4)
+    print(final)
+
+
+def get_bitcoin():
+    try:
+        response = requests.get("https://api.coinpaprika.com/v1/tickers/btc-bitcoin")
+        response.raise_for_status()
+    except requests.RequestException:
+        print("Error fetching bitcoin price")
+        return None
+    except ValueError:
+        print("Error: invalid JSON response")
+    data = response.json()
+    price = data["quotes"]["USD"]["price"]
+    return price
+
+
+def get_n():
+    try:
+        n = float(sys.argv[1])
+        return n
+    except ValueError:
+        sys.exit("Error: please enter a number")
+
+
+main()
