e80d7765325f8984be98b1690b9447344a9f2cfe
FHawthorne <flynn.hawthorne@education.nsw.gov.au>
2026-03-02 22:39:23 +0000

    Upload WIP shirt.py

--- 6-FileIO/shirt/shirt.py
+++ 6-FileIO/shirt/shirt.py
@@ -1,17 +1,27 @@
-import PIL
+from PIL import Image, ImageOps
 import sys
+from pathlib import Path
 
 
 def main():
-    input = sys.argv[1]
-    output = sys.argv[2]
 
-    shirt = image.open(input)
-    size = shirt.size
-    PIL.imageOps.fit(image: shirt, size: tuple[size] 
-                    method: int = Resampling.BICUBIC, 
-                    bleed: float = 0.0, centering: 
-                    tuple[float, float] = (0.5, 0.5)) 
+    if len(sys.argv) > 3:
+        sys.exit("Must include an input and output file")
+    if len(sys.argv) < 3:
+        sys.exit("Must include an input and output file")
+
+    input = Path(sys.argv[1])
+    output = Path(sys.argv[2])
+    input = input.lower()
+    output = output.lower()
+    try:
+        with Image.open(input) as input, Image.open("shirt.png") as shirt:
+            size = shirt.size
+            fitted = ImageOps.fit(input, size)
+            fitted.paste(shirt, (0, 0), shirt)
+            fitted.save(output)
+    except (FileNotFoundError, ValueError):
+        sys.exit("file could not be found")
 
 
 if __name__ == "__main__":
